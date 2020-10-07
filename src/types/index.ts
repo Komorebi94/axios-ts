dcd8c66a305f6dcb9230cfe31ab2a649bb9cc256
lujuxiao <lujuxiao@ljxdeMacBook-Pro.local>
2020-10-07 08:54:16 +0800

    fitst commit

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface AxiosRequestConfig {
+    url: string
+    method?: Method
+    data?: any
+    params?: any
+}
+
+export type Method = 'get' | 'get' | 'GET' | 'delete' | 'Delete' | 'head' | 'HEAD' | 'options' | 'OPTIONS' | 'post' | 'POST' | 'put' | 'PUT' | 'patch' | 'PATCH';
